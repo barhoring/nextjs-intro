9829e5c673e383a28d04ebb7efffb6e00c6484ab
iambar <bar.apple@gmail.com>
2024-11-02 19:14:31 +0200

    with some help from the docs

--- app/docs/[[...id]]/page.tsx
+++ app/docs/[[...id]]/page.tsx
@@ -1,5 +1,14 @@
-const DocsPage = ({ params }) => {
-  return <div>Docs Page {JSON.stringify(params)}</div>
+type Props = {
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+const DocsPage = ({ params, searchParams }: Props) => {
+  return (
+    <div>
+      Docs Page {JSON.stringify(params)} {JSON.stringify(searchParams)}
+    </div>
+  )
 }
 
 export default DocsPage
